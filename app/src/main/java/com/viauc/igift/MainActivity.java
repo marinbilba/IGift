026435c1018934b9e2647ff93b045e52e504985a
Marin Bilba <marynbilba@gmail.com>
2021-04-14 19:14:26 +0200

    Finished log in UI design

--- app/src/main/java/com/viauc/igift/MainActivity.java
+++ app/src/main/java/com/viauc/igift/MainActivity.java
@@ -3,6 +3,7 @@ package com.viauc.igift;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signInUser(View view) {
+    }
+
+    public void signUpUser(View view) {
     }
 }
