6193af6aa950e4deaad21f85651b09dc86bd3f5c
Marin Bilba <marynbilba@gmail.com>
2021-05-03 11:57:59 +0200

    start calendar implementation

---
+++ app/src/main/java/com/viauc/igift/ui/calendar/CalendarFragment.java
@@ -0,0 +1,29 @@
+package com.viauc.igift.ui.calendar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.viauc.igift.R;
+
+public class CalendarFragment extends Fragment {
+
+    private CalendarViewModel calendarViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        calendarViewModel =
+                new ViewModelProvider(this).get(CalendarViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        return root;
+    }
+}
